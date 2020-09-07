c3767baabcc7d892a52196a10d0b460e0777c96b
rafael <rafael@DESKTOP-3P5M8LL>
2020-09-06 21:13:31 -0300

    implementação de filme

--- src/sistema/Filme.java
+++ src/sistema/Filme.java
@@ -1,5 +1,22 @@
 package sistema;
 
+import java.time.YearMonth;
+import java.util.List;
+
+import profissoes.*;
+
 public class Filme {
+	private String titulo;
+	private YearMonth ano;
+	private List<Ator> elenco;
+	private List<Musica> trilhaSonora;
+	private Funcionario diretor;
+	private Funcionario roteirista;
+	private Funcionario cinegrafista;
+	
+	public Filme(String titulo, YearMonth ano) {
+		this.titulo = titulo;
+		this.ano = ano;
+	}
 
 }
