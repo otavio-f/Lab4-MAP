8cfbf56d4ad661c853551a0e77158eeb186c10f7
Otavio <Otavio@RADINHO2>
2020-09-07 08:05:04 -0300

    Remoção de método não usado

--- src/sistema/Funcionario.java
+++ src/sistema/Funcionario.java
@@ -22,14 +22,6 @@ public class Funcionario {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
